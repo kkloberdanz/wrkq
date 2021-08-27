4dfda60a41c8ecbd5cc3d4441b3a4227fde6ec12
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2021-08-26 21:11:11 -0500

    added job enqueueing and dequeueing

--- include/wrkq.h
+++ include/wrkq.h
@@ -1,6 +1,10 @@
 #ifndef WRKQ_H
 #define WRKQ_H
 
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
 int foo(void);
 
 struct wrkq_item {
@@ -8,10 +12,15 @@ struct wrkq_item {
      * this function gets invoked with the argument 'arg' once it gets pulled
      * from the work queue
      */
-    void (*func)(void *);
+    void *(*func)(void *);
 
     /* input argument to func */
     void *arg;
+};
+
+struct wrkq_result {
+    void *value;
+    size_t id;
 };
 
 struct wrkq_options {
@@ -19,19 +28,17 @@ struct wrkq_options {
     size_t n_workers;
 
     /* maximum number of items in the work queue */
-    size_t q_depth;
+    size_t queue_depth;
 };
 
-struct wrkq_t {
-    struct wrkq_item *queue;
-    pthread_t *threads;
-    size_t n_workers;
-    size_t q_depth;
-    size_t q_index;
-};
+struct wrkq_t;
 
 struct wrkq_t *wrkq_new(struct wrkq_options *opt);
 
 void wrkq_destroy(struct wrkq_t *q);
+
+size_t wrkq_nq(struct wrkq_t *q, struct wrkq_item *item);
+
+void wrkq_dq(struct wrkq_t *q, struct wrkq_result *out);
 
 #endif /* WRKQ_H */
--- src/wrkq.c
+++ src/wrkq.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 #include <wrkq.h>
+
+struct wrkq_t {
+    struct wrkq_item *queue;
+    struct wrkq_result *results;
+    pthread_t *threads;
+    size_t n_workers;
+    size_t queue_depth;
+    size_t queue_index;
+    size_t result_index;
+    size_t id;
+    pthread_mutex_t mtx;
+    sem_t empty_count;
+    sem_t fill_count;
+
+};
+
+static void pull_job(struct wrkq_t *q, struct wrkq_item *out);
+static void push_result(struct wrkq_t *q, struct wrkq_result *result);
 
 int foo(void) {
     puts("called foo");
@@ -12,90 +29,89 @@ int foo(void) {
 /*
  * TODO: create ring buffer
  */
-/*
- * Producer-consumer model
- *
- * The locking gets pretty hairy here, best to use an existing algorithm
- * that is known to work (see multi producer/consumer example)
- * https://en.wikipedia.org/wiki/Producer%E2%80%93consumer_problem#Using_semaphores
- */
-static void *producer(void *args) {
-#define UNUSED(A) (void)(A)
-    UNUSED(args);
-#undef UNUSED
+
+static void *wrkq_thread_loop(void *args) {
+    struct wrkq_t *q = args;
+    struct wrkq_item work_item;
+    void *result = NULL;
+
     for (;;) {
-        /*
-         * data = produce_item()
-         * sem_wait(empty_count)
-         * lock(buffer_mutex)
-         * put_item_in_buffer(data)
-         * unlock(buffer_mutex)
-         * sem_post(fill_count)
-         */
+        pull_job(q, &work_item);
+        result = work_item.func(work_item.arg);
+        push_result(q, result);
     }
     return NULL;
 }
 
-static void *consumer(void) {
-    /*
-     * sem_wait(fill_count)
-     * lock(buffer_mutex)
-     * item = remove_item_from_buffer()
-     * unlock(buffer_mutex)
-     * sem_post(empty_count)
-     * return item
-     */
-    return NULL;
-}
+static int init_locks(struct wrkq_t *q) {
+    int err = 0;
 
-static void *wrkq_thread_loop(void *args) {
-    struct work_t *q = args;
-#define UNUSED(A) (void)(A)
-    UNUSED(q);
-#undef UNUSED
-    /* TODO: wait for ring buffer to unlock */
-    return NULL;
+    err = pthread_mutex_init(&q->mtx, NULL);
+    if (err) {
+        perror("failed to initialize mutex");
+        goto error;
+    }
+
+    err = sem_init(&q->empty_count, 0, q->queue_depth);
+    if (err) {
+        goto destroy_empty_count;
+    }
+
+    err = sem_init(&q->fill_count, 0, 0);
+    if (err) {
+        goto destroy_fill_count;
+    }
+
+destroy_fill_count:
+    sem_destroy(&q->fill_count);
+destroy_empty_count:
+    sem_destroy(&q->empty_count);
+error:
+    pthread_mutex_destroy(&q->mtx);
+    return 1;
 }
 
 struct wrkq_t *wrkq_new(struct wrkq_options *opt) {
     struct wrkq_item *queue = NULL;
     struct wrkq_t *q = NULL;
     pthread_t *threads = NULL;
-    size_t i;
+    size_t n_started = 0;
+    size_t i = 0;
 
-#define UNUSED(A) (void)(A)
-    UNUSED(consumer);
-    UNUSED(producer);
-#undef UNUSED
-    
     q = malloc(sizeof(*q));
     if (!q) {
         goto fail;
     }
     q->n_workers = opt->n_workers;
-    q->q_depth = opt->q_depth;
+    q->queue_depth = opt->queue_depth;
+    q->id = 0;
 
-    queue = malloc(sizeof(*queue) * opt->q_depth);
+    queue = malloc(sizeof(*queue) * opt->queue_depth);
     if (!queue) {
         goto fail;
     }
     q->queue = queue;
-    q->q_index = 0;
+    q->queue_index = 0;
 
-    threads = malloc(sizeof(*threads) * opt->n_workers);
+    threads = calloc(opt->n_workers, sizeof(*threads));
     if (!threads) {
         goto fail;
     }
     q->threads = threads;
 
-    /* TODO: setup ring buffer */
-    for (i = 0; i < opt->n_workers; i++) {
-        int status = pthread_create(&threads[i], NULL, wrkq_thread_loop, NULL);
+    init_locks(q);
+    for (n_started = 0; n_started < opt->n_workers; n_started++) {
+        int status = pthread_create(
+            &threads[n_started],
+            NULL,
+            wrkq_thread_loop,
+            NULL
+        );
         if (status) {
             perror("wrkq: failed to create thread");
-            goto fail; /* TODO: how to handle failed threads? */
+            goto fail;
         } else {
-            pthread_detach(threads[i]);
+            pthread_detach(threads[n_started]);
         }
     }
     
@@ -104,11 +120,71 @@ struct wrkq_t *wrkq_new(struct wrkq_options *opt) {
 fail:
     free(q);
     free(queue);
+    for (i = 0; i < n_started; i++) {
+        pthread_cancel(threads[i]);
+    }
     free(threads);
     return NULL;
 }
 
 void wrkq_destroy(struct wrkq_t *q) {
+    size_t i;
     free(q->queue);
+    for (i = 0; i < q->n_workers; i++) {
+        pthread_cancel(q->threads[i]);
+    }
+    free(q->threads);
     free(q);
+}
+
+/*
+ * Producer-consumer model
+ *
+ * The locking gets pretty hairy here, best to use an existing algorithm
+ * that is known to work (see multi producer/consumer example)
+ * https://en.wikipedia.org/wiki/Producer%E2%80%93consumer_problem#Using_semaphores
+ */
+static size_t job_enqueue(struct wrkq_t *q, struct wrkq_item *item) {
+    q->queue[q->queue_index] = *item;
+    q->queue_index = (q->queue_index + 1) % q->queue_depth;
+    q->id++;
+    return q->id;
+}
+
+static void job_dequeue(struct wrkq_t *q, struct wrkq_item *out) {
+    *out = q->queue[q->queue_index];
+    q->queue_index = (q->queue_index + 1) % q->queue_depth;
+}
+
+size_t wrkq_nq(struct wrkq_t *q, struct wrkq_item *item) {
+    size_t id = 0;
+
+    sem_wait(&q->empty_count);
+    pthread_mutex_lock(&q->mtx);
+    id = job_enqueue(q, item);
+    pthread_mutex_unlock(&q->mtx);
+    sem_post(&q->fill_count);
+    return id;
+}
+
+void wrkq_dq(struct wrkq_t *q, struct wrkq_result *out) {
+#define UNUSED(A) (void)(A)
+    UNUSED(q);
+    UNUSED(out);
+#undef UNUSED
+}
+
+static void pull_job(struct wrkq_t *q, struct wrkq_item *out) {
+    sem_wait(&q->fill_count);
+    pthread_mutex_lock(&q->mtx);
+    job_dequeue(q, out);
+    pthread_mutex_unlock(&q->mtx);
+    sem_post(&q->empty_count);
+}
+
+static void push_result(struct wrkq_t *q, struct wrkq_result *result) {
+#define UNUSED(A) (void)(A)
+    UNUSED(q);
+    UNUSED(result);
+#undef UNUSED
 }
